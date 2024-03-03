2d3243e456663ca7423957fd27c27c7726c1ddff
Nemesai38 <pappysouls@gmail.com>
2024-03-03 08:17:03 +0100

    radio added, pic, db, size upgraded

--- personnel_info.py
+++ personnel_info.py
@@ -2,18 +2,131 @@ import tkinter
 from tkinter import ttk
 from tkinter import *
 import sqlite3
+import os
 from PIL import ImageTk, Image
 from tkinter import messagebox
 from tkinter import filedialog
 
-# Defining  necessary image variables
-img_name = ""
-per_image = ['images/blank.jpg']
+
+# Main Window
+window = tkinter.Tk()
+window.title("Personnel Entry Form")
+window.geometry("1150x650")
+window.resizable(False, False)
+
+# Main Frame
+frame = tkinter.Frame(window)
+frame.pack()
+
+
+# Function to select image file from os
+# and display on form selected image
+def upload_img():
+    global img_label
+    global filename
+    filename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select Photo File", filetypes=(("JPG File", "*.jpg"),
+                                                                                                        ("PNG File", "*.png"),
+                                                                                                        ("All Files", "*.txt")))
+    img_name = Image.open(filename)
+    resized_photo = img_name.resize((300, 310))
+    photo2 = ImageTk.PhotoImage(resized_photo)
+    img_label.config(image=photo2)
+    img_label = photo2
+
+
+def clear_image():
+    global img_label
+    img_label = Label(per_img_frame, image=photo)
+    img_label.place(x=0, y=0)
+
+
+def gender_selection():
+    global gender
+    value = radio.get()
+    if value == 1:
+        gender = "Male"
+    else:
+        gender = "Female"
+
+
+def marital_status_check():
+    global marital_stats
+    value1 = radio.get()
+    if value1 == 1:
+        marital_stats = "Single"
+    elif value1 == 2:
+        marital_stats = "Married"
+    elif value1 == 3:
+        marital_stats = "Divorced"
+
+
+# Function to collect personnel information into database
+# and create one if one doesn't exist
+def reg_per():
+    if army_no_entry.get() == "" or rank_entry.get() == "" or first_name_entry.get() == "" or last_name_entry.get()\
+       == "" or unit_entry.get() == "" or corps_entry.get() == "" or appt_entry.get() == "" or deploy_entry.get()\
+       == "" or state_entry.get() == "" or trade_entry.get() == "" or address_entry.get("1.0", 'end') == "":
+        tkinter.messagebox.showinfo(title="ERROR", message="Please all fields are required to be filled")
+    else:
+        # User Info
+        try:
+            with open(filename, 'rb') as file:
+                photo_image = file.read()
+        except NameError:
+            pass
+        army_number = army_no_entry.get()
+        rank = rank_entry.get()
+        firstname = first_name_entry.get()
+        lastname = last_name_entry.get()
+        try:
+            sex = gender
+        except NameError:
+            tkinter.messagebox.showerror("Error", "Please select Gender")
+        unit = unit_entry.get()
+        corps = corps_entry.get()
+        appointment = appt_entry.get()
+        deployment = deploy_entry.get()
+        state = state_entry.get()
+        trade = trade_entry.get()
+        address = address_entry.get("1.0", 'end-1c')
+        try:
+            marital_status = marital_stats
+        except NameError:
+            tkinter.messagebox.showerror("Error", "Please Select Marital Status")
+        try:
+            image = photo_image
+        except NameError:
+            image = ""
+
+        # Create Table
+        conn = sqlite3.connect('data.db')
+        table_create_query = '''CREATE TABLE IF NOT EXISTS Personnel_Info (serial INTEGER PRIMARY KEY AUTOINCREMENT, armynumber TEXT, 
+        rank TEXT, firstname TEXT, lastname TEXT, sex TEXT, unit TEXT, corps TEXT, appointment TEXT, deployment TEXT, state TEXT, 
+        trade TEXT, address TEXT, maritalstatus TEXT, image BLOB)
+        '''
+        conn.execute(table_create_query)
+
+        # Insert Data
+        data_insert_query = '''INSERT INTO Personnel_Info (armynumber, rank, firstname, lastname, sex, unit, corps, appointment,
+        deployment, state, trade, address, maritalstatus, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+        data_insert_tuple = (army_number, rank, firstname, lastname, sex, unit, corps, appointment, deployment, state, trade,
+                             address, marital_status, image,)
+        cursor = conn.cursor()
+        cursor.execute(data_insert_query, data_insert_tuple)
+        conn.commit()
+        conn.close()
+
+        # Notify info entry success
+        tkinter.messagebox.showinfo(title="DONE", message="Personnel has been registered successfully")
+
+        # Clear fields
+        clear_all_fields()
+        return
 
 
 # Function to clear all input fields at will
 def clear_all_fields():
-    global img
+    global img_label
     army_no_entry.delete(0, 'end')
     rank_entry.set('')
     first_name_entry.delete(0, 'end')
@@ -25,79 +138,9 @@ def clear_all_fields():
     state_entry.set('')
     trade_entry.delete(0, 'end')
     address_entry.delete('1.0', 'end')
-    status_entry.set('')
-    img = ""
+    img_label = Label(per_img_frame, image=photo)
+    img_label.place(x=0, y=0)
     return
-
-
-def clear_image():
-    global img
-    img = ""
-
-
-# Function to collect personnel information into database
-# and create one if one doesn't exist
-def reg_per():
-    global img
-    if army_no_entry.get() == "" or rank_entry.get() == "" or first_name_entry.get() == "" or last_name_entry.get()\
-        == "" or unit_entry.get() == "" or corps_entry.get() == "" or appt_entry.get() == "" or deploy_entry.get()\
-        == "" or state_entry.get() == "" or trade_entry.get() == "" or address_entry.get("1.0", 'end') == "" or\
-            status_entry.get() == "" or img_name == "":
-        tkinter.messagebox.showinfo(title="ERROR", message="Please all fields are required to be filled")
-    else:
-        # User Info
-        with open(img_name, 'rb') as file:
-            photo_image = file.read()
-        armynumber = army_no_entry.get()
-        rank = rank_entry.get()
-        firstname = first_name_entry.get()
-        lastname = last_name_entry.get()
-        unit = unit_entry.get()
-        corps = corps_entry.get()
-        appointment = appt_entry.get()
-        deployment = deploy_entry.get()
-        state = state_entry.get()
-        trade = trade_entry.get()
-        address = address_entry.get("1.0", 'end-1c')
-        maritalstatus = status_entry.get()
-        image = photo_image
-
-        # Create Table
-        conn = sqlite3.connect('data.db')
-        table_create_query = '''CREATE TABLE IF NOT EXISTS Personnel_Info (armynumber TEXT, rank TEXT, firstname TEXT,
-        lastname TEXT, unit TEXT, corps TEXT, appointment TEXT, deployment TEXT, state TEXT, trade TEXT, 
-        address TEXT, maritalstatus TEXT, image BLOB)
-        '''
-        conn.execute(table_create_query)
-
-        # Insert Data
-        data_insert_query = '''INSERT INTO Personnel_Info (armynumber, rank, firstname, lastname, unit, corps, appointment,
-        deployment, state, trade, address, maritalstatus, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'''
-        data_insert_tuple = (armynumber, rank, firstname, lastname, unit, corps, appointment, deployment, state, trade,
-                             address, maritalstatus, image)
-        cursor = conn.cursor()
-        cursor.execute(data_insert_query, data_insert_tuple)
-        conn.commit()
-        conn.close()
-
-        # Notify info entry success
-        tkinter.messagebox.showinfo(title="DONE", message="Personnel has been registered successfully")
-
-        # Clear fields
-        army_no_entry.delete(0, 'end')
-        rank_entry.set('')
-        first_name_entry.delete(0, 'end')
-        last_name_entry.delete(0, 'end')
-        unit_entry.delete(0, 'end')
-        corps_entry.delete(0, 'end')
-        appt_entry.delete(0, 'end')
-        deploy_entry.delete(0, 'end')
-        state_entry.set('')
-        trade_entry.delete(0, 'end')
-        address_entry.delete('1.0', 'end')
-        status_entry.set('')
-        img = ""
-        return
 
 
 # Function to return to DB Hub (close dialog box at the moment)
@@ -106,33 +149,9 @@ def go_back():
     pass
 
 
-# Main Window
-window = tkinter.Tk()
-window.title("Personnel Entry Form")
-window.geometry("900x600")
-window.resizable(False, False)
-
-# Main Frame
-frame = tkinter.Frame(window)
-frame.pack()
-
-
-# Function to select image file from os
-# and display on form selected image
-def upload_img():
-    global per_image
-    global img
-    global img_name
-    img_name = filedialog.askopenfilename()
-    per_image.append(img_name)
-    per_img_frame_canvas.pack()
-    img = ImageTk.PhotoImage(Image.open(per_image[len(per_image) - 1]))
-    per_img_frame_canvas.create_image(20, 20, anchor='nw', image=img)
-
-
 # Frame to collect all required information
 per_info_frame = tkinter.LabelFrame(frame, text="Personnel Information")
-per_info_frame.grid(row=0, column=0, padx=20, pady=10, ipady=10)
+per_info_frame.grid(row=0, column=0)
 
 # Labels and Entries
 army_no_label = tkinter.Label(per_info_frame, text="Army Number")
@@ -156,6 +175,22 @@ unit_label = tkinter.Label(per_info_frame, text="Unit")
 unit_label.grid(row=0, column=4)
 unit_entry = tkinter.Entry(per_info_frame)
 unit_entry.grid(row=1, column=4)
+gender_label = tkinter.Label(per_info_frame, text="Gender")
+gender_label.grid(row=0, column=5)
+radio = IntVar()
+R1 = Radiobutton(per_info_frame, text="Male", variable=radio, value=1, command=gender_selection)
+R1.grid(row=1, column=5, sticky='w')
+R2 = Radiobutton(per_info_frame, text="Female", variable=radio, value=2, command=gender_selection)
+R2.grid(row=2, column=5, sticky='w')
+status_label = tkinter.Label(per_info_frame, text="Marital Status")
+status_label.grid(row=0, column=6)
+radio1 = IntVar()
+R3 = Radiobutton(per_info_frame, text="Single", variable=radio1, value=1, command=marital_status_check)
+R3.grid(row=1, column=6, sticky='w')
+R4 = Radiobutton(per_info_frame, text="Married", variable=radio1, value=2, command=marital_status_check)
+R4.grid(row=2, column=6, sticky='w')
+R5 = Radiobutton(per_info_frame, text="Divorced", variable=radio1, value=3, command=marital_status_check)
+R5.grid(row=3, column=6, sticky='w')
 corps_label = tkinter.Label(per_info_frame, text="Corps")
 corps_label.grid(row=2, column=0)
 corps_entry = tkinter.Entry(per_info_frame)
@@ -183,39 +218,30 @@ trade_entry = tkinter.Entry(per_info_frame)
 trade_entry.grid(row=3, column=4)
 address_label = tkinter.Label(per_info_frame, text="Accommodation Address")
 address_label.grid(row=4, column=0, columnspan=2)
-address_entry = Text(per_info_frame, width=10, height=10, padx=5, pady=5, wrap=WORD)
+address_entry = Text(per_info_frame, width=10, height=10, padx=3, pady=2, wrap=WORD)
 address_entry.grid(row=5, column=0, columnspan=2, sticky="ew")
-status_label = tkinter.Label(per_info_frame, text="Marital Status")
-status_label.grid(row=4, column=2)
-status_entry = ttk.Combobox(per_info_frame, values=["Single", "Married", "Divorced"], state="readonly")
-status_entry.grid(row=5, column=2, sticky='n')
-per_img_label = tkinter.Label(per_info_frame, text="Personnel Photo")
-per_img_label.grid(row=4, column=3, columnspan=2)
-per_img_frame = tkinter.LabelFrame(per_info_frame, text='Photo', width=300, height=250, borderwidth=3, relief=GROOVE)
-per_img_frame.grid(row=5, column=3, columnspan=2)
-per_img_frame_canvas = tkinter.Canvas(per_img_frame, bg='grey', width=300, height=250, scrollregion=(0, 0, 500, 500))
-per_img_frame_canvas.pack(expand=True, fill='both')
-per_img_frame_canvas.bind('<Button-4>', lambda event: per_img_frame_canvas.yview_scroll(int(event.y / 90), "units"))
-per_img_frame_canvas.bind('<Button-5>', lambda event: per_img_frame_canvas.yview_scroll(-int(event.y / 90), "units"))
-per_img_frame_canvas_scrollbar = ttk.Scrollbar(per_img_frame, orient='vertical', command=per_img_frame_canvas.yview)
-per_img_frame_canvas_scrollbar_btn = ttk.Scrollbar(per_img_frame, orient='horizontal', command=per_img_frame_canvas.xview)
-per_img_frame_canvas.configure(yscrollcommand=per_img_frame_canvas_scrollbar.set,
-                               xscrollcommand=per_img_frame_canvas_scrollbar_btn.set)
-per_img_frame_canvas_scrollbar.place(relx=1, rely=0, relheight=1, anchor='ne')
-per_img_frame_canvas_scrollbar_btn.place(relx=0, rely=1, relwidth=1, anchor='sw')
+per_img_frame = Frame(per_info_frame, bg="black", width=300, height=310, borderwidth=3, relief=GROOVE)
+per_img_frame.grid(row=5, column=4, columnspan=2)
+img = Image.open("fotos/passport.png")
+resized_image = img.resize((300, 310))
+photo = ImageTk.PhotoImage(resized_image)
+img_label = Label(per_img_frame, image=photo)
+img_label.place(x=0, y=0)
 
 
 # Buttons
-upload_image_btn = tkinter.Button(per_info_frame, text="Upload\nImage", command=upload_img)
-upload_image_btn.grid(row=5, column=2, sticky='e')
-clear_image_btn = tkinter.Button(per_info_frame, text="Clear\nImage", command=clear_image)
-clear_image_btn.grid(row=5, column=2, sticky='se')
-clear_button = tkinter.Button(frame, text="CLEAR ALL", command=clear_all_fields)
-register_personnel = tkinter.Button(frame, text="REGISTER PERSONNEL", command=reg_per)
-return_button = tkinter.Button(frame, text="Go Back", command=go_back)
-clear_button.grid(row=1, column=0, sticky='news', padx=20, pady=2)
-register_personnel.grid(row=2, column=0, sticky='news', padx=20, pady=2)
-return_button.grid(row=3, column=0, sticky='news', padx=20, pady=2)
+upload_image_btn = tkinter.Button(per_info_frame, text="Upload\nImage", command=upload_img, bg='lightblue')
+upload_image_btn.grid(row=5, column=3, sticky='e')
+clear_image_btn = tkinter.Button(per_info_frame, text="Clear\nImage", command=clear_image, bg='lightpink')
+clear_image_btn.grid(row=5, column=3, sticky='se')
+clear_button = tkinter.Button(per_info_frame, text="CLEAR ALL", command=clear_all_fields, bg='#eb8080')
+clear_button.grid(row=6, column=0, sticky='news', padx=20, pady=2)
+register_personnel = tkinter.Button(per_info_frame, text="REGISTER PERSONNEL", command=reg_per, bg='lightgreen')
+register_personnel.grid(row=7, column=0, sticky='news', padx=20, pady=2)
+update_button = tkinter.Button(per_info_frame, text="UPDATE INFORMATION", bg='#a980eb')
+update_button.grid(row=8, column=0, sticky='news', padx=20, pady=2)
+return_button = tkinter.Button(per_info_frame, text="Go Back", command=go_back, bg="#ce7e00")
+return_button.grid(row=9, column=0, sticky='news', padx=20, pady=2)
 
 for widget in per_info_frame.winfo_children():
     widget.grid_configure(padx=10, pady=2)
--- view_per_info.py
+++ view_per_info.py
@@ -7,8 +7,8 @@ import io
 conn = sqlite3.connect("data.db")
 c = conn.cursor()
 c.execute("SELECT * FROM Personnel_Info")
-i = 0
 r = c.fetchall()
+i = 0
 
 
 app = CTk()
@@ -22,7 +22,7 @@ heading = CTkLabel(master=app, font=("Roboto", 17, "italic"), text="Personnel In
 heading.place(x=90, y=5)
 
 # Image
-photo = r[i][12]
+photo = r[i][14]
 # convert image data to file object
 fp = io.BytesIO(photo)
 per_image = CTkImage(dark_image=Image.open(fp), size=(200, 200))
@@ -37,22 +37,22 @@ def next_per():
     global fp
     global per_image
     i += 1
-    photo = r[i][12]
+    photo = r[i][14]
     fp = io.BytesIO(photo)
     per_image = CTkImage(dark_image=Image.open(fp), size=(200, 200))
     image_label.configure(image=per_image)
-    army_number_label.configure(text=r[i][0])
-    rank_label.configure(text=r[i][1])
-    first_name_label.configure(text=r[i][2])
-    last_name_label.configure(text=r[i][3])
-    per_state_label.configure(text=r[i][8])
-    per_unit_label.configure(text=r[i][4])
-    per_corps_label.configure(text=r[i][5])
-    per_deployment_label.configure(text=r[i][7])
-    per_appointment_label.configure(text=r[i][6])
-    per_trade_label.configure(text=r[i][9])
-    per_marital_status_label.configure(text=r[i][11])
-    per_address_label.configure(text=r[i][10])
+    army_number_label.configure(text=r[i][1])
+    rank_label.configure(text=r[i][2])
+    first_name_label.configure(text=r[i][3])
+    last_name_label.configure(text=r[i][4])
+    per_state_label.configure(text=r[i][10])
+    per_unit_label.configure(text=r[i][6])
+    per_corps_label.configure(text=r[i][7])
+    per_deployment_label.configure(text=r[i][9])
+    per_appointment_label.configure(text=r[i][8])
+    per_trade_label.configure(text=r[i][11])
+    per_marital_status_label.configure(text=r[i][13])
+    per_address_label.configure(text=r[i][12])
 
 
 def go_back():
@@ -71,47 +71,47 @@ exit_btn = CTkButton(master=app, text="GO BACK", corner_radius=32, fg_color="#81
 exit_btn.place(x=215, y=200)
 
 # Labels
-army_number_label = CTkLabel(master=app, font=("Roboto", 19, "italic", "bold"), text=r[i][0], text_color="#ba862c")
-army_number_label.place(x=10, rely=.4)
-rank_label = CTkLabel(master=app, font=("Roboto", 19, "italic", "bold"), text=r[i][1], text_color="#ba862c")
-rank_label.place(x=10, rely=.45)
-first_name_label = CTkLabel(master=app, font=("Roboto", 19, "italic", "bold"), text=r[i][2], text_color="#ba862c")
-first_name_label.place(x=10, rely=.5)
-last_name_label = CTkLabel(master=app, font=("Roboto", 19, "italic", "bold"), text=r[i][3], text_color="#ba862c")
-last_name_label.place(relx=.45, rely=.5)
+army_number_label = CTkLabel(master=app, font=("Roboto", 19, "italic", "bold"), text=r[i][1], text_color="#ba862c")
+army_number_label.place(x=10, rely=.38)
+rank_label = CTkLabel(master=app, font=("Roboto", 19, "italic", "bold"), text=r[i][2], text_color="#ba862c")
+rank_label.place(x=10, rely=.43)
+first_name_label = CTkLabel(master=app, font=("Roboto", 19, "italic", "bold"), text=r[i][3], text_color="#ba862c")
+first_name_label.place(x=10, rely=.48)
+last_name_label = CTkLabel(master=app, font=("Roboto", 19, "italic", "bold"), text=r[i][4], text_color="#ba862c")
+last_name_label.place(relx=.45, rely=.48)
 state_label = CTkLabel(master=app, font=("Roboto", 17, "italic", "bold"), text="State:", text_color="#ba862c")
-state_label.place(x=10, rely=.55)
-per_state_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][8], text_color="#ba862c")
-per_state_label.place(x=90, rely=.55)
+state_label.place(x=10, rely=.53)
+per_state_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][10], text_color="#ba862c")
+per_state_label.place(x=90, rely=.53)
 unit_label = CTkLabel(master=app, font=("Roboto", 17, "italic", "bold"), text="Unit:", text_color="#ba862c")
-unit_label.place(x=10, rely=.6)
-per_unit_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][4], text_color="#ba862c")
-per_unit_label.place(x=90, rely=.6)
+unit_label.place(x=10, rely=.58)
+per_unit_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][6], text_color="#ba862c")
+per_unit_label.place(x=90, rely=.58)
 corps_label = CTkLabel(master=app, font=("Roboto", 17, "italic", "bold"), text="Corps:", text_color="#ba862c")
-corps_label.place(x=10, rely=.65)
-per_corps_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][5], text_color="#ba862c")
-per_corps_label.place(x=90, rely=.65)
+corps_label.place(x=10, rely=.63)
+per_corps_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][7], text_color="#ba862c")
+per_corps_label.place(x=90, rely=.63)
 deployment_label = CTkLabel(master=app, font=("Roboto", 17, "italic", "bold"), text="Deployment:", text_color="#ba862c")
-deployment_label.place(x=10, rely=.7)
-per_deployment_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][7], text_color="#ba862c")
-per_deployment_label.place(x=150, rely=.7)
+deployment_label.place(x=10, rely=.68)
+per_deployment_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][9], text_color="#ba862c")
+per_deployment_label.place(x=150, rely=.68)
 appointment_label = CTkLabel(master=app, font=("Roboto", 17, "italic", "bold"), text="Appointment:", text_color="#ba862c")
-appointment_label.place(x=10, rely=.75)
-per_appointment_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][6], text_color="#ba862c")
-per_appointment_label.place(x=150, rely=.75)
+appointment_label.place(x=10, rely=.73)
+per_appointment_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][8], text_color="#ba862c")
+per_appointment_label.place(x=150, rely=.73)
 trade_label = CTkLabel(master=app, font=("Roboto", 17, "italic", "bold"), text="Trade:", text_color="#ba862c")
-trade_label.place(x=10, rely=.8)
-per_trade_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][9], text_color="#ba862c")
-per_trade_label.place(x=90, rely=.8)
+trade_label.place(x=10, rely=.78)
+per_trade_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][11], text_color="#ba862c")
+per_trade_label.place(x=90, rely=.78)
 marital_status_label = CTkLabel(master=app, font=("Roboto", 17, "italic", "bold"), text="Marital Status:", text_color="#ba862c")
-marital_status_label.place(x=10, rely=.85)
-per_marital_status_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][11], text_color="#ba862c")
-per_marital_status_label.place(x=165, rely=.85)
+marital_status_label.place(x=10, rely=.83)
+per_marital_status_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][13], text_color="#ba862c")
+per_marital_status_label.place(x=165, rely=.83)
 address_label = CTkLabel(master=app, font=("Roboto", 17, "italic", "bold"), text="Accommodation:", text_color="#ba862c")
-address_label.place(x=10, rely=.895)
-per_address_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][10],
+address_label.place(x=10, rely=.865)
+per_address_label = CTkLabel(master=app, font=("Roboto", 17, "italic"), text=r[i][12],
                              text_color="#ba862c", wraplength=200, justify='left')
-per_address_label.place(x=165, rely=.9)
+per_address_label.place(x=165, rely=.87)
 
 
 app.mainloop()
